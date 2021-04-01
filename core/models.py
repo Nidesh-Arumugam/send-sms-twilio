5ca7ef2a1a9aa9dac8aa6937d078d39dcadcfa60
Nidesh-Arumugam <80671776+Nidesh-Arumugam@users.noreply.github.com>
2021-04-01 13:41:24 +0530

    Update models.py
    just added the desc

--- core/models.py
+++ core/models.py
@@ -10,16 +10,16 @@ class Core(models.Model):
         return str(self.result)
 
     def save(self,*args, **kwargs):
-        if self.result<70:
-            account_sid = 'AC58f02d80fa415fc0b1c6ba35035b06d1'
-            auth_token = 'e8c96f185a92565ed1b8294a4a794bae'
+        if self.result<70:                                        //this is just an sample if case, if we add a number less than 70 in Admin section the message will be sent.
+            account_sid = 'AC58f02d80fa415fc0b1c6ba35035b06d1'   //code from twilio
+            auth_token = 'e8c96f185a92565ed1b8294a4a794bae'      //account_sid and account_token are created at time of sign in in twilio
             client = Client(account_sid, auth_token)
 
             message = client.messages.create(
                                         body='Hi There !',
                                         from_='+18328502576',
                                         to='Reciever Number',
-                                        )
+                                        )                       //code ends from twilio
 
             print(message.sid)
         return super().save(*args, **kwargs)
